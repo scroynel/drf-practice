14b0512e97c8153d97a160d524af6fbbfacadfae
Vladyslav <scroynel@gmail.com>
2026-07-16 21:33:13 +0200

    Change models file

--- drf-travel-project-rewrite/travel/models.py
+++ drf-travel-project-rewrite/travel/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class TravelProject(models.Model):
+    name = models.CharField(max_length=100, unique=True, db_index=True)
+    description = models.TextField(blank=True, null=True)
+    start_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Place(models.Model):
+    external_id = models.IntegerField(db_index=True)
+    name = models.CharField(max_length=100)
+
+
+class TravelProjectPlace(models.Model):
+    project = models.ForeignKey(TravelProject, on_delete=models.CASCADE, related_name='places')
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+    notes = models.TextField(blank=True, null=True)
+    visited = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
