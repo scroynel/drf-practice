34b6fde89f4c3537f2dd00cf649c8512bb2856d6
Vladyslav <scroynel@gmail.com>
2026-07-16 19:13:23 +0200

    Add .env file with database data and implementing JWT Token

--- drf-travel-project-rewrite/project/settings.py
+++ drf-travel-project-rewrite/project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'travel'
 ]
 
@@ -130,3 +131,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
